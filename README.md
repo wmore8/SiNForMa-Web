d3b1a53cc02a37b8c6ba2eb7355e3c6549e3f4ee
Washing-Mr8 <washing.22ml@gmail.com>
2026-03-11 00:30:38 +0100

    PWA funcional en prototipo

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 **ToDo:**
 
 - Actualizar el `readme`
- -  **Prototipo** - En Proceso
+ -  **Prototipo** -> Listo: [https://cool-marshmallow-512ba5.netlify.app](https://cool-marshmallow-512ba5.netlify.app/)
  - Spinner
  - Unidad de conversión (Romesco a Decimal)
  - Tablas
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['sprite.svg'], // Archivos estaticos offline
+      manifest: {
+        name: 'Sinforma Web Proto',
+        short_name: 'Sinforma',
+        description: 'Actividad Tablas y PWA proto ',
+        theme_color: '#32DE84',
+        background_color: '#f0fdf4',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/',
+        id: '/',
+        icons: [
+          {
+            src: 'proto_icon_x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+
+          },
+          {
+            src: 'proto_icon_x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
